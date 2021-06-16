dbfc8e7317053d3879c54c005f4eb6ac802222a4
Stephen Messham <Messham87@gmail.com>
2021-06-16 22:15:12 +0000

    fixed train to run on colab (Parser and issues with torch.data[0])

--- train.py
+++ train.py
@@ -17,52 +17,50 @@ from utils import load_data, accuracy
 from models import GAT, SpGAT
 
 # Training settings
-parser = argparse.ArgumentParser()
-parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
-parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
-parser.add_argument('--sparse', action='store_true', default=False, help='GAT with sparse version or not.')
-parser.add_argument('--seed', type=int, default=72, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
-parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
-parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
-parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
-parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
-parser.add_argument('--patience', type=int, default=100, help='Patience')
+nocuda = False
+fastmode = False
+sparse = False
+seed = 72
+epochs = 10000
+lr = 0.005
+weight_decay = 5e-4
+hidden = 8
+nb_heads = 8
+dropout = 0.6
+alpha = 0.2
+patience = 100
 
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
+cuda = not nocuda and torch.cuda.is_available()
 
-random.seed(args.seed)
-np.random.seed(args.seed)
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+if cuda:
+    torch.cuda.manual_seed(seed)
 
 # Load data
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
 
 # Model and optimizer
-if args.sparse:
+if sparse:
     model = SpGAT(nfeat=features.shape[1], 
-                nhid=args.hidden, 
+                nhid=hidden, 
                 nclass=int(labels.max()) + 1, 
-                dropout=args.dropout, 
-                nheads=args.nb_heads, 
-                alpha=args.alpha)
+                dropout=dropout, 
+                nheads=nb_heads, 
+                alpha=alpha)
 else:
     model = GAT(nfeat=features.shape[1], 
-                nhid=args.hidden, 
+                nhid=hidden, 
                 nclass=int(labels.max()) + 1, 
-                dropout=args.dropout, 
-                nheads=args.nb_heads, 
-                alpha=args.alpha)
+                dropout=dropout, 
+                nheads=nb_heads, 
+                alpha=alpha)
 optimizer = optim.Adam(model.parameters(), 
-                       lr=args.lr, 
-                       weight_decay=args.weight_decay)
+                       lr=lr, 
+                       weight_decay=weight_decay)
 
-if args.cuda:
+if cuda:
     model.cuda()
     features = features.cuda()
     adj = adj.cuda()
@@ -84,7 +82,7 @@ def train(epoch):
     loss_train.backward()
     optimizer.step()
 
-    if not args.fastmode:
+    if not fastmode:
         # Evaluate validation set performance separately,
         # deactivates dropout during validation run.
         model.eval()
@@ -108,16 +106,16 @@ def compute_test():
     loss_test = F.nll_loss(output[idx_test], labels[idx_test])
     acc_test = accuracy(output[idx_test], labels[idx_test])
     print("Test set results:",
-          "loss= {:.4f}".format(loss_test.data[0]),
-          "accuracy= {:.4f}".format(acc_test.data[0]))
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
 
 # Train model
 t_total = time.time()
 loss_values = []
 bad_counter = 0
-best = args.epochs + 1
+best = epochs + 1
 best_epoch = 0
-for epoch in range(args.epochs):
+for epoch in range(epochs):
     loss_values.append(train(epoch))
 
     torch.save(model.state_dict(), '{}.pkl'.format(epoch))
@@ -128,7 +126,7 @@ for epoch in range(args.epochs):
     else:
         bad_counter += 1
 
-    if bad_counter == args.patience:
+    if bad_counter == patience:
         break
 
     files = glob.glob('*.pkl')
