bdf832406029dcdd66ed54252c852b86b83148a5
Stephen Messham <Messham87@gmail.com>
2021-10-24 17:32:38 +0100

    updated model

--- models.py
+++ models.py
@@ -189,10 +189,13 @@ class TwoLayerGATMLP(nn.Module):
         # x = torch.flatten(x)
         x = self.lin1(x)
         x = torch.sigmoid(x)
+        x = F.dropout(x)
         x = self.lin2(x)
         x = torch.sigmoid(x)
+        x = F.dropout(x)
         x = self.lin3(x)
         x = torch.sigmoid(x)
+        x = F.dropout(x)
         x = self.lin4(x)
         return F.relu(x)
 
